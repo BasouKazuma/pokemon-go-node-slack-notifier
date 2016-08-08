4c9ce7301454c27173775c4433ce8bc1a805e80b
Abbas Jaffery <Abbas Jaffery>
2016-08-08 15:57:56 -0700

    Reauthenticates upon lookup error in order to allow the app to run uninterrupted

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var Pokeio = require('pokemon-go-node-api');
+var PokemonApi = require('pokemon-go-node-api');
 
 // Required user generated config file
 var config = require('./config.json');
@@ -23,16 +23,19 @@ var heartbeatTimeInterval = 10 * 1000;
 
 // Functions
 
-var postToSlack = function(url, data)
+var postToSlack = function(slack_url, nearby_pokemon_fields)
 {
+    var slack_data = {
+        attachments: nearby_pokemon_fields
+    };
     request.post(
         {
-            url: url,
+            url: slack_url,
             json: true,
             headers: {
                "content-type": "application/json",
             },
-            body: data
+            body: slack_data
         }, 
         function(error, response, body) {
             console.log(body);
@@ -94,85 +97,60 @@ var removeExpiredPokemon = function(discovered_pokemon)
     return discovered_pokemon;
 }
 
-var findPokemon = function(pokeio_instance, config) {
-    pokeio_instance.Heartbeat(function(err,hb) {
-        var current_time_object = new Date();
-        current_time = current_time_object.getTime();
-        console.log("*** NEW RUN @" + current_time + " ***");
-        if (err)
-        {
-            console.log(err);
-            if (err == 'No result')
-            {
-                // Try to log back in
-                pokeio_instance.init(config.username, config.password, config.location, config.provider, function(err) {
-                    if (err)
-                    {
-                        console.log(err);
-                    }
-                });
-            }
-        }
-        else
-        {
-            var fallback_text = "Nearby Pokemon: ";
-            var nearby_pokemon_fields = [];
+var findPokemon = function(hb) {
+    var fallback_text = "Nearby Pokemon: ";
+    var nearby_pokemon_fields = [];
 
-            for (var i = hb.cells.length - 1; i >= 0; i--)
+    for (var i = hb.cells.length - 1; i >= 0; i--)
+    {
+    if (hb.cells[i].WildPokemon[0])
+    {
+        for (var j = hb.cells[i].WildPokemon.length - 1; j >= 0; j--)
+        {
+            var wildPokemon = hb.cells[i].WildPokemon[j];
+            var pokemon = pokeio_instance.pokemonlist[parseInt(wildPokemon.pokemon.PokemonId)-1];
+            console.log('[i] There is a ' + pokemon.name + ' nearby');
+            
+            var notify_pokemon = true;
+            
+            for (var k = 0; k < pokemon_ignore_list.length; k++)
             {
-                if (hb.cells[i].WildPokemon[0])
+              if (pokemon_ignore_list[k] == pokemon.id)
+              {
+                notify_pokemon = false;
+              }
+            }
+            
+            for (var m = discovered_pokemon.length - 1; m >= 0; m--)
+            {
+                if (discovered_pokemon[m].encounter_id.low == wildPokemon.EncounterId.low &&
+                    discovered_pokemon[m].encounter_id.high == wildPokemon.EncounterId.high &&
+                    discovered_pokemon[m].encounter_id.unsigned == wildPokemon.EncounterId.unsigned)
                 {
-                    for (var j = hb.cells[i].WildPokemon.length - 1; j >= 0; j--)
-                    {
-                        var wildPokemon = hb.cells[i].WildPokemon[j];
-                        var pokemon = pokeio_instance.pokemonlist[parseInt(wildPokemon.pokemon.PokemonId)-1];
-                        console.log('[i] There is a ' + pokemon.name + ' nearby');
-                        
-                        var notify_pokemon = true;
-                        
-                        for (var k = 0; k < pokemon_ignore_list.length; k++)
-                        {
-                          if (pokemon_ignore_list[k] == pokemon.id)
-                          {
-                            notify_pokemon = false;
-                          }
-                        }
-                        
-                        for (var m = discovered_pokemon.length - 1; m >= 0; m--)
-                        {
-                            if (discovered_pokemon[m].encounter_id.low == wildPokemon.EncounterId.low &&
-                                discovered_pokemon[m].encounter_id.high == wildPokemon.EncounterId.high &&
-                                discovered_pokemon[m].encounter_id.unsigned == wildPokemon.EncounterId.unsigned)
-                            {
-                                notify_pokemon = false;
-                            }
-                        }
-                        
-                        if (notify_pokemon == true)
-                        {
-                            fallback_text += pokemon.name + ' |';
-                            nearby_pokemon_fields = addNearbyPokemon(nearby_pokemon_fields, pokemon, wildPokemon, pokevision_url, google_maps_url);
-                            discovered_pokemon = addDiscoveredPokemon(discovered_pokemon, pokemon, wildPokemon);
-                        }
-                    } 
+                    notify_pokemon = false;
                 }
             }
-
-            var slackData = {
-              attachments: nearby_pokemon_fields
-            };
             
-            postToSlack(config.slack_request_url, slackData);
-            discovered_pokemon = removeExpiredPokemon(discovered_pokemon);
+            if (notify_pokemon == true)
+            {
+                fallback_text += pokemon.name + ' |';
+                nearby_pokemon_fields = addNearbyPokemon(nearby_pokemon_fields, pokemon, wildPokemon, pokevision_url, google_maps_url);
+                discovered_pokemon = addDiscoveredPokemon(discovered_pokemon, pokemon, wildPokemon);
+            }
+        } 
+    }
+}
+if (nearby_pokemon_fields.length > 0)
+{
+    postToSlack(config.slack_request_url, nearby_pokemon_fields);
+}
+discovered_pokemon = removeExpiredPokemon(discovered_pokemon);
 
-        }
-
-    });
 };
 
 // App logic
 
-var pokeio_instance = Pokeio;
+var pokeio_instance = new PokemonApi.Pokeio();
 pokeio_instance.init(config.username, config.password, config.location, config.provider, function(err) {
     if (err)
     {
@@ -181,7 +159,27 @@ pokeio_instance.init(config.username, config.password, config.location, config.p
 });
 
 setInterval(function() {
-    findPokemon(pokeio_instance, config);
+    pokeio_instance.Heartbeat(function(err,hb) {
+        var current_time_object = new Date();
+        current_time = current_time_object.getTime();
+        console.log("*** NEW RUN @" + current_time + " ***");
+        if (err)
+        {
+            console.log(err);
+            // Try to log back in
+            pokeio_instance = new PokemonApi.Pokeio();
+            pokeio_instance.init(config.username, config.password, config.location, config.provider, function(err) {
+                if (err)
+                {
+                    console.log(err);
+                }
+            });
+        }
+        else
+        {
+            findPokemon(hb);
+        }
+    });
 }, heartbeatTimeInterval);
 
 
